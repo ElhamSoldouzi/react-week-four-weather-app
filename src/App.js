98fe27b687a999ae6ec8fcacabedc7c92dbca4ea
ElhamSoldouzi <134371922+ElhamSoldouzi@users.noreply.github.com>
2025-10-29 18:15:22 +0330

    adding footer and links to github and vercel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This project was coded by Elham Soldouzi and it is open-sourced on{" "}
+        <a
+          href="https://github.com/ElhamSoldouzi/react-week-four-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and deployed on{" "}
+        <a
+          href="https://react-week-four-weather-app.vercel.app/"
+          terget="_blank"
+          rel="noreferrer"
+        >
+          Vercel
+        </a>
+      </footer>
+    </div>
+  );
 }
